3ba3e782f8fd4bb48eb1a6d145a1c4d182fb7074
Juan Samuel Vargas <juan.samuel.vargas@gmail.com>
2016-12-06 13:19:34 -0800

    Add name to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
 
-  get '/mision-vision-principios', to: 'static_pages#mvp'
-  get '/mensaje-de-la-direccion', to: 'static_pages#mensaje_direccion'
-  get '/logros', to: 'static_pages#logros'
-  get '/reconocimientos-y-certificaciones', to: 'static_pages#ryc'
-  get '/ram', to: 'static_pages#ram'
-  get '/tijuana-waterkeeper', to: 'static_pages#tj_waterkeeper'
-  get '/corredores-ecologicos-transfronterizos', to: 'static_pages#cet'
-  get '/salvemos-la-playa', to: 'static_pages#salvemos_playa'
-  get '/eventos', to: 'static_pages#eventos'
-  get '/donativos', to: 'static_pages#donativos'
+  get '/mision-vision-principios' => 'static_pages#mvp', as: 'mvp'
+  get '/mensaje-de-la-direccion' => 'static_pages#mensaje_direccion', as: 'mensaje_direccion'
+  get '/logros' => 'static_pages#logros', as: 'logros'
+  get '/reconocimientos-y-certificaciones' => 'static_pages#ryc', as: 'ryc'
+  get '/ram' => 'static_pages#ram', as: 'ram'
+  get '/tijuana-waterkeeper' => 'static_pages#tj_waterkeeper', as: 'tj_waterkeeper'
+  get '/corredores-ecologicos-transfronterizos' => 'static_pages#cet', as: 'cet'
+  get '/salvemos-la-playa' => 'static_pages#salvemos_playa', as: 'salvemos_playa'
+  get '/eventos' => 'static_pages#eventos', as: 'eventos'
+  get '/donativos' => 'static_pages#donativos', as: 'donativos'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

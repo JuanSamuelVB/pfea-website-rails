ad3bcfdb295c2a61c7e41f5d5b62e884095fa203
Juan Samuel Vargas <juan.samuel.vargas@gmail.com>
2016-12-06 00:25:15 -0800

    Add app stylesheets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,12 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( pfea.css )
+Rails.application.config.assets.precompile += %w( pfea.colors.css )
+Rails.application.config.assets.precompile += %w( pfea.footer.css )
+Rails.application.config.assets.precompile += %w( pfea.icons.css )
+Rails.application.config.assets.precompile += %w( pfea.images.css )
+Rails.application.config.assets.precompile += %w( pfea.navbar.css )
+Rails.application.config.assets.precompile += %w( pfea.parallax.css )
+Rails.application.config.assets.precompile += %w( pfea.widgets.css )
